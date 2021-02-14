8f5a24cd05052ad9eb41da78b5344cdaf0f48e3d
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2021-02-14 22:37:06 +0100

    hello world working locally
    both https .onResquest() (with CORS) and .onCall() work.
    However firebase deploy bugs, due to some incompatibilities with locally installed nodejs version 15.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+exports.helloWorld = functions.https.onRequest( ( request, response )=>{
+  cors( request, response, ()=>{
+    functions.logger.info( "helloWorld() logs!", { structuredData: true } );
+    response.json( { data: "HhlloWorld() from Tweesic firebase nodejs side!" } );
+  });
+});
+
+// Callable function
+exports.hello = functions.https.onCall( ( data, context ) => {
+  functions.logger.info( "hello() logs!", { structuredData: true } );
+  return "hello() from Tweesic firebase nodejs side!";
+});
